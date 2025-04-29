619efd9c823f78516269b74fa0b1aeb2ddc056e0
Youmi <youmi.ngs@gmail.com>
2025-04-29 22:14:22 +0900

    Add PostQuestionView and PostWorkView

---
+++ Shaka/PostQuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  PostQuestionView.swift
+//  Shaka
+//
+//  Created by Youmi Nagase on 2025/04/29.
+//
+
+import SwiftUI
+
+struct PostQuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostQuestionView()
+}
